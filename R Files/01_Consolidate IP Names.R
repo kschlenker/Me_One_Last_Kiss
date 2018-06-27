abd1e3420133f3906531f7235e01a7e94b1e5269
Aaron Chafetz <achafetz@usaid.gov>
2018-06-27 16:40:17 -0400

    remove tolower as default in read_msd, no more need for COP Matrix (API pull) and write as RDS to save space

--- R Files/01_Consolidate IP Names.R
+++ R Files/01_Consolidate IP Names.R
@@ -1,12 +1,11 @@
 # How to use "rename official" to consolidate IPs and change all their names to the most current version.
-# NOTE: You must have the COP matrix report in the same folder. 
 
 
-df<-read_msd(datafile,datapath)
+df<-read_msd(file.path(datapath,datafile))
 
-df <- dplyr::rename_all(df, ~ tolower(.))
+#df <- dplyr::rename_all(df, ~ tolower(.)) #default with read_msd
 
-df_final<- rename_official(df,datapath)
-write_tsv(df_final, file.path(datapath, "ICPI_MER_Structured_Dataset_PSNU_IM_FY17-18_20180515_v1_1_FV_Clean.txt"))
+df_final<- rename_official(df)
+write_rds(df_final, file.path(datapath, "ICPI_MER_Structured_Dataset_PSNU_IM_FY17-18_20180515_v1_1_FV_Clean.rds"))
 
 rm(df, df_final)
