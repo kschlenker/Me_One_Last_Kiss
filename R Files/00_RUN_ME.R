e0f158854de6b3841eb7080c7c678698a6fd31ec
Aaron Chafetz <achafetz@usaid.gov>
2018-06-28 15:19:14 -0400

    remove magrittr, added datafile_archive for FY16Q4 TX_CURR import

--- R Files/00_RUN_ME.R
+++ R Files/00_RUN_ME.R
@@ -3,19 +3,19 @@
 
 # INSTALL LIBRARIES
 devtools::install_github("ICPI/ICPIutilities")
-pacman::p_load("tidyverse", "magrittr", "ICPIutilities")
+pacman::p_load("tidyverse", "ICPIutilities")
 
 # INCREASE MEMORY
 memory.limit(size=56000)
 
 # FILE PATHS
-datapath<-"~/Me_One_Last_Kiss/Data"
+datapath<-"~/GitHub/Me_One_Last_Kiss/Data"
 #scriptpath <- "~/GitHub/Me_One_Last_Kiss/R Files"
 #setwd(scriptpath)
 
 # NAME STRUCTURED DATASET
 datafile<- "ICPI_MER_Structured_Dataset_PSNU_IM_FY17-18_20180515_v1_1.txt"
-
+datafile_archive <- "ICPI_MER_Structured_Dataset_PSNU_IM_FY15-16_20180515_v1_1.txt"
 # YOU NEED TO RUN 01_Consolidate IP Names.R ONLY ONCE TO CREATE THE "CLEAN" DATASET.
 # ONCE YOU HAVE RUN IT ONE TIME AND HAVE THE CLEAN FILE, YOU CAN START DIRECTLY WITH 02_FY18Q2Tableau.R
 
