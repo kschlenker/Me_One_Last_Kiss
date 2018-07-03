59804d1b54b2517f653778301fc4ef3f14d49516
Aaron Chafetz <achafetz@usaid.gov>
2018-07-03 16:01:56 -0400

    detail and specify character cols

--- R Files/06_reshape_long.R
+++ R Files/06_reshape_long.R
@@ -1,23 +1,29 @@
 #convert specified data type (results/targets/apr) to long
 
 reshape_long <- function(df, type){
-  if(type == "apr" || type == "targets") {
-    cols <- df %>% 
-      select(ends_with(type)) %>% 
+  #identify value columsn to pull
+    if(type == "apr" || type == "targets") {
+      cols <- df %>% 
+        select(ends_with(type)) %>% 
+        names()
+    } else if (type == "results") {
+      cols <- df %>% 
+        select(contains("q")) %>% 
+        names()
+    } else
+      stop("type is not correctly specified")
+  
+  #keep string columns already filtered
+    lst_meta <- df %>%
+      dplyr::select_if(is.character) %>%
       names()
-  } else if (type == "results") {
-    cols <- df %>% 
-      select(contains("q")) %>% 
-      names()
-  } else
-    stop("type is not correctly specified")
-
-  df <- df %>%
-    select(TableauColumns, cols) %>% 
-    group_by_at(TableauColumns) %>%
-    summarize_at(vars(starts_with("fy2")), funs(sum(., na.rm=TRUE))) %>%
-    ungroup %>%
-    gather(period, values, starts_with("fy2")) %>%
-    filter(values !=0)
+    
+  #aggregate and convert to long
+    df <- df %>%
+      group_by_at(lst_meta) %>%
+      summarize_at(vars(starts_with("fy2")), funs(sum(., na.rm=TRUE))) %>%
+      ungroup %>%
+      gather(period, values, starts_with("fy2")) %>%
+      filter(values !=0)
 }
 
