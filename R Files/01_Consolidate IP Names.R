b11a70841f0a5808443f525ec7ef9ea18c961610
Aaron Chafetz <achafetz@usaid.gov>
2018-07-03 12:18:32 -0400

    update for q2clean uids, aggregate and added gz compression

--- R Files/01_Consolidate IP Names.R
+++ R Files/01_Consolidate IP Names.R
@@ -12,24 +12,30 @@
   
 #limit just to just meta data (string vars), excluding partner/mech and other UIDs that may lead to misalignment in merge
   lst_meta <- df_tx_old %>% 
-    #select(-c(dataelementuid, categoryoptioncombouid, primepartner, implementingmechanismname)) %>% 
+    select(-c(dataelementuid, categoryoptioncombouid)) %>% 
     select_if(is.character) %>% 
     names()
   df_tx_old <- select(df_tx_old, lst_meta, fy2016q4)
   
+  df_tx_old <- df_tx_old %>% 
+    group_by_if(is.character) %>% 
+    summarise(fy2016q4 = sum(fy2016q4, na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    filter(fy2016q4 != 0)
+  
 #join archive data onto current dataset
-  df <- full_join(df, df_tx_old)
+  df_merge <- full_join(df, df_tx_old)
   
 #reorder so FY16Q4 comes before FY17
-  lst_meta <- df %>% 
+  lst_meta <- df_merge %>% 
     select_if(is.character) %>% 
     names()
-  df <- select(df, lst_meta, fy2016q4, everything())
+  df_merge <- select(df_merge, lst_meta, fy2016q4, everything())
 
 #apply "offical" names to partners/implementing mechs
   df_final<- rename_official(df)
 
 #export as RDS
-  write_rds(df_final, file.path(datapath, "ICPI_MER_Structured_Dataset_PSNU_IM_FY17-18_20180515_v1_1_FV_Clean.rds"))
+  write_rds(df_final, file.path(datapath, "ICPI_MER_Structured_Dataset_PSNU_IM_FY17-18_20180515_v2_1_FV_Clean.rds"), compress = "gz")
 
-rm(df, df_final, df_tx_old, lst_meta)
+rm(df, df_final, df_tx_old, df_merge, lst_meta)
