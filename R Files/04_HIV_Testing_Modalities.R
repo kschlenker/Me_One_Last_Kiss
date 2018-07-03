28fdff8a42f00f423b3adebc48a81d456d0b2a37
Aaron Chafetz <achafetz@usaid.gov>
2018-07-03 15:40:29 -0400

    modalities simplified

--- R Files/04_HIV_Testing_Modalities.R
+++ R Files/04_HIV_Testing_Modalities.R
@@ -4,26 +4,17 @@
 # ____________________________________
 
 finaldata <- finaldata %>% 
-  mutate(modality = case_when(modality == "OtherMod"                                                 ~ "Other Community",
-                               modality == "IndexMod"                                                 ~ "Community Index",
-                               modality == "VCTMod"                                                   ~ "Community VCT",
-                               modality %in% c("HomeMod", "Home-based")                               ~ "Community Home-Based",
-                               modality %in% c("MobileMod", "Mobile")                                 ~ "Community Mobile",
-                               modality %in% c("VCT", "Voluntary Counseling & Testing standalone", 
-                                               "Voluntary Counseling & Testing co-located")           ~ "VCT",
-                               modality %in% c("VMMC", "Voluntary Medical Male Circumcision")         ~ "VMMC",
-                               modality %in% c("Other PITC", "OtherPITC", "Other Service Delivery Point")           ~ "Other PITC",
-                               modality %in% c("Index", "Index Testing")                              ~ "Index",
-                               modality %in% c("PMTCT", "PMTCT ANC", "Antenatal Clinic")              ~ "PMTCT (ANC)",
-                               modality %in% c("Pediatric", "Pediatrics","Under 5 Clinic")            ~ "Pediatrics",
-                               modality %in% c("TBClinic", "Tuberculosis")                            ~ "TB",
-                               modality %in% c("Malnutrition", "Malnutrition facilities")             ~ "Malnutrition",
-                               modality == "STI Clinic"                                               ~ "STI",
-                               modality %in% c("Inpat", "Inpatient")                                  ~ "Inpatient",
-                               modality %in% c("Emergency", "Emergency Ward")                         ~ "Emergency",
-                               modality == "Outpatient Department"                                    ~ "Outpatient",
-                               modality == "Labor & Delivery"                                         ~ "L&D",
-                               modality == "HIV Care and Treatment Clinic"                            ~ "Care and Treatment",
-                               modality == "KeyPop"                                                   ~ "Key Populations"
-  )) 
-
+  mutate(modality = case_when(modality == "Emergency Ward"                     ~ "Emergency",
+                              modality == "HomeMod"                            ~ "Community Home-Based",
+                              modality == "IndexMod"                           ~ "Community Index",
+                              modality == "Inpat"                              ~ "Inpatient",
+                              modality == "MobileMod"                          ~ "Community Mobile",
+                              modality == "OtherMod"                           ~ "Other Community",
+                              modality == "OtherPITC"                          ~ "Other PITC",
+                              modality == "PMTCT ANC"                          ~ "PMTCT (ANC)",
+                              modality == "STI Clinic"                         ~ "STI",
+                              modality == "TBClinic"                           ~ "TB",
+                              modality == "VCTMod"                             ~ "Community VCT",
+                              modality %in% c("Index" , "Malnutrition", 
+                                              "Pediatric", "VCT", "VMMC")      ~ modality)
+  ) 
